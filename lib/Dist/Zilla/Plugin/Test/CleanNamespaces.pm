0fc0ce7d67628af112eebedcf6d9a9f4f2fe2824
Karen Etheridge <ether@cpan.org>
2015-09-05 11:33:17 -0700

    this section is now generated by my weaver bundle

--- lib/Dist/Zilla/Plugin/Test/CleanNamespaces.pm
+++ lib/Dist/Zilla/Plugin/Test/CleanNamespaces.pm
@@ -168,14 +168,6 @@ be used more than once.
 * use of a configurable L<FileFinder|Dist::Zilla::Role::FileFinder> for finding
 source files to check (depends on changes planned in L<Test::CleanNamespaces>)
 
-=head1 SUPPORT
-
-=for stopwords irc
-
-Bugs may be submitted through L<the RT bug tracker|https://rt.cpan.org/Public/Dist/Display.html?Name=Dist-Zilla-Plugin-Test-CleanNamespaces>
-(or L<bug-Dist-Zilla-Plugin-Test-CleanNamespaces@rt.cpan.org|mailto:bug-Dist-Zilla-Plugin-Test-CleanNamespaces@rt.cpan.org>).
-I am also usually active on irc, as 'ether' at C<irc.perl.org>.
-
 =head1 SEE ALSO
 
 =for :list
